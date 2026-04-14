d2c2b07757819843bb7c905c9d8ef211f8043523
kaplancali-dev <kaplancali@icloud.com>
2026-04-13 20:21:12 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🌿 Vegan Recipe Finder — Service Worker
-const VERSION = "vrf-20260414n";
+const VERSION = "vrf-20260414o";
 
 self.addEventListener("install", e => {
   self.skipWaiting(); // activate immediately
