a4e73c0621bf1836f2f23ea1775a12dc994b4d57
David Kaplan <davidkaplan@Mac.lan>
2026-04-14 20:11:03 -0700

    Bump SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🌿 Vegan Recipe Finder — Service Worker
-const VERSION = "vrf-20260416l";
+const VERSION = "vrf-20260416m";
 
 self.addEventListener("install", e => {
   self.skipWaiting(); // activate immediately
