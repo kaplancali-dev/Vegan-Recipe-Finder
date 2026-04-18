814b050a4d2852f6f2f6f7ae4dae76cd7018a49f
David Kaplan <kaplancali@icloud.com>
2026-04-18 12:05:46 -0700

    Change recipe count to 'at least X you can make right now'

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🌿 HARVEST — Service Worker
-const VERSION = "vrf-20260418q";
+const VERSION = "vrf-20260418r";
 
 self.addEventListener("install", e => {
   self.skipWaiting(); // activate immediately
